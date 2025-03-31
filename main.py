8f3a515fb6fcdfd7a1448facde45448622a87343
thebestcatdad <thebestcatdadcoding@gmail.com>
2025-03-31 17:59:57 -0500

    added book text fetch and word count

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("greetings boots")
+def get_book_text(bookfile):
+    file_contents = None
+    with open(bookfile) as f:
+        file_contents = f.read()
+    return file_contents
+
+def count_words(bookfile):
+    word_count = 0
+    book_text = get_book_text(bookfile)
+    for i in book_text.split():
+        word_count += 1
+    return word_count
+
+def main():
+    print(f"{count_words("books/frankenstein.txt")} words found in the document")
+    
+main()
