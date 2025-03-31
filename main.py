e5e00cde6f41d63c73b4bad9c10f3e081d89c7b7
thebestcatdad <thebestcatdadcoding@gmail.com>
2025-03-31 18:13:24 -0500

    refactored main

--- main.py
+++ main.py
@@ -5,13 +5,11 @@ def get_book_text(bookfile):
     return file_contents
 
 def count_words(bookfile):
-    word_count = 0
     book_text = get_book_text(bookfile)
-    for i in book_text.split():
-        word_count += 1
+    word_count = len(book_text.split())
     return word_count
 
 def main():
-    print(f"{count_words("books/frankenstein.txt")} words found in the document")
+    print(f"{count_words('books/frankenstein.txt')} words found in the document")
     
 main()
