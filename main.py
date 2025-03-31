991cb726edd26f958f3774a3bc94dea06143605c
thebestcatdad <thebestcatdadcoding@gmail.com>
2025-03-31 18:30:26 -0500

    separated stats from main

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
+from stats import get_num_words
+
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    count_words = get_count_words(text)
+    count_words = get_num_words(text)
     print(f"{count_words} words found in the document")
 
 def get_book_text(path):
      with open(path) as f:
         return f.read()
 
-def get_count_words(text):
-    word_count = text.split()
-    return len(word_count)
-    
 main()
---
+++ stats.py
@@ -0,0 +1,4 @@
+def get_num_words(text):
+    word_count = text.split()
+    return len(word_count)
+    
