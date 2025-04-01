79eecc5f611a6fe3b4da7ae12b07665bd43e09f6
thebestcatdad <thebestcatdadcoding@gmail.com>
2025-03-31 19:22:34 -0500

    added letter count

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from stats import get_num_words
+from stats import get_num_letters
 
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     count_words = get_num_words(text)
     print(f"{count_words} words found in the document")
+    count_letters = get_num_letters(text)
+    print(f"{count_letters} of each letter found in the document")
 
 def get_book_text(path):
      with open(path) as f:
--- stats.py
+++ stats.py
@@ -2,3 +2,12 @@ def get_num_words(text):
     word_count = text.split()
     return len(word_count)
     
+def get_num_letters(text):
+    letter_count = {}
+    for char in text:
+        char = char.lower()
+        if char in letter_count:
+            letter_count[char] += 1
+        else:
+            letter_count[char] = 1
+    return letter_count
