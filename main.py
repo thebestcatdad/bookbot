8ce9f9d89d497b80e94720340a751827dd837833
thebestcatdad <thebestcatdadcoding@gmail.com>
2025-04-02 09:03:12 -0500

    added cli argument to read files

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
+import sys
 from stats import get_num_words
 from stats import get_num_letters
 from stats import dictionary_to_list
 
 def main():
-    book_path = "books/frankenstein.txt"
+    book_path = get_book_path(sys.argv)
     text = get_book_text(book_path)
     count_words = get_num_words(text)
     count_letters = get_num_letters(text)
     sorted_list = dictionary_to_list(count_letters)
     print_results(book_path, count_words, sorted_list)
+
+def get_book_path(path):
+    if len(path) <= 1:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        return path[1]
+
 
 def get_book_text(path):
      with open(path) as f:
